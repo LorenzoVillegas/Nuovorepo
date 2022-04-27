924b8ec152fc29952efd928c270d4f1ce47953cc
LorenzoVillegas <villegas.lorenzo@iisgalvanimi.edu.it>
2022-04-27 09:54:27 +0200

    prova

--- js/script.js
+++ js/script.js
@@ -14,26 +14,46 @@ let minute=0;
 let hour=0
 let myInterval;
 let barLength=1;
+let attivo;
 
 // --- FUNCTIONs
 function startCounter(){
+ 
 
-  if (typeof(myInterval) != "undefined") { return ; }
+  if (typeof(myInterval) == "undefined" || attivo == true) { 
+    
+    return myInterval = setInterval(conteggioFunc, 1000);
+    
+   }
+    
   // continua tu
 }
 
 function stopCounter(){
+  clearInterval(myInterval)
+  
+  if (typeof(myInterval) != "undefined") { 
+    
+    return attivo = false; }
 
-  if (typeof(myInterval) == "undefined") { return ; }
+
   // continua tu
 }
 
 function conteggioFunc(){
   second++
+  domSecond.innerText = second
   if (second >= 60) {
     second=0
     minute++
-  }
+    domMinute.innerText = minute
+  } 
+
+  if (minute >=60) {
+    minute = 0
+    hour++
+    domHour.innerText = hour
+    }
    // continua tu
    console.log("tic")
 }
